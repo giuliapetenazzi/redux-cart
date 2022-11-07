705916c41e03b76d80863b92c70b1f4c7224f681
LAPTOP-SORM8G9A\Giulia <giuliaanno95@hotmail.it>
2022-11-07 23:10:35 +0100

    added Notification management

--- src/App.js
+++ src/App.js
@@ -2,16 +2,60 @@ import React from "react";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
-import { useDispatch, useSelector } from "react-redux";
+import Notification from "./components/Notification";
+import uiActions from "./store/ui-slice";
+
+import { useDispatch, useSelector} from "react-redux";
+import { useEffect } from "react";
+
+//let isFirstRender = true;
 
 function App() {
-
+  const dispatch = useDispatch();
+  const notification = useSelector(state => state.ui.notification);
+  /*
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   const cartItems = useSelector((state) => state.cart.itemsList);
   console.log("🚀 ~ cartItems", cartItems)
+  */
 
+  const cart = useSelector((state) => state.cart);
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn); 
+  useEffect(()=> {
+    /*if (isFirstRender) {
+      isFirstRender = false;
+      return;
+    }*/
+    const sendRequest = async () => {
+      dispatch(uiActions.showNotification({
+        open: true,
+        message: "Sending request",
+        type: 'warning'
+      }));
+      const res = await fetch('', {
+        method: 'PUT',
+        body: JSON.stringify(cart),
+      });
+      const data = await res.json();
+      uiActions.showNotification({
+        open: true,
+        message: "Sent request to database successfully",
+        type: 'success'
+      })
+    };
+    sendRequest().catch((err) => {
+      dispatch(uiActions.showNotification({
+        open: true,
+        message: "Sending requestfailed",
+        type: 'error'
+      }));
+    });
+  });
   return (
     <div className="App">
+        { notification && 
+          <Notification type={notification.type} message={notification.message} />
+        }
       { !isLoggedIn && <Auth /> }
       { isLoggedIn && <Layout /> }
     </div>
